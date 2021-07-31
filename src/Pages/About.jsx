6928acf31905483ba0ba46f033d1653b3bb477d2
Shawlycode <kutinaorlando@gmail.com>
2021-07-31 13:36:31 +0000

    added about component

---
+++ src/Pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+             <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+        voluptatibus officiis nam accusamus saepe culpa soluta dicta incidunt,
+        fugiat quos, quae quasi corrupti illum tempora, tempore necessitatibus
+        perspiciatis. Sequi, mollitia.
+      </p>
+        </div>
+    )
+}
+
+export default About
