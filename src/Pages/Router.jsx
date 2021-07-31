cda88320734d2d2a4e2e3c7dd9cdff9e6be559ad
Shawlycode <kutinaorlando@gmail.com>
2021-07-31 14:15:50 +0000

    added inline styles

--- src/Pages/Router.jsx
+++ src/Pages/Router.jsx
@@ -12,13 +12,15 @@ function Router() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar bg="dark" variant="dark">
-          <Container>
+        <Navbar bg="dark" variant="dark" >
+          <Container className="Nav">
             <Nav className="me-auto">
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-              <Link to="/services">Services</Link>
+              <div className="nav-links">
+              <Link to="/" style={{marginRight:"50px", textDecoration:"none"}}>Home</Link>
+              <Link to="/about" style={{marginRight:"50px", textDecoration:"none"}}>About</Link>
+              <Link to="/contact" style={{marginRight:"50px" , textDecoration:"none"}}>Contact</Link>
+              <Link to="/services" style={{marginRight:"50px", textDecoration:"none"}}>Services</Link>
+              </div>
             </Nav>
           </Container>
         </Navbar>
